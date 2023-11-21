c5bca773872d90d4778b30ae89d0c7ca9da1b336
ravishankar01661 <138732855+ravishankar01661@users.noreply.github.com>
2023-11-21 12:56:10 +0530

    Add files via upload

---
+++ C++/Patterns/Solid Square.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int size = 5; // Change the size of the square pattern
+
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+

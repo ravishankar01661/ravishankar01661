779d6eb118cd7d96d935b00967e5a5b1ad2f33cf
ravishankar01661 <138732855+ravishankar01661@users.noreply.github.com>
2024-04-26 11:38:22 +0530

    Add files via upload

---
+++ React/Project/Simple Counter/App.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import Counter from './counter';
+
+
+function App() {
+    return (
+        <div className="App">
+            <Counter />
+        </div>
+    );
+}
+
+export default App;

36d9c49ea986fb0837dcdb3188f9c10dfd8577d8
ravishankar01661 <138732855+ravishankar01661@users.noreply.github.com>
2024-04-26 13:34:55 +0530

    Add files via upload

---
+++ React/Accordian/Single Selection/index.jsx
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+import data from './data';
+import './styles.css'
+
+
+export default function Accordian() {
+    const [selected, setSelected] = useState(null);
+
+    function handleSingleSelection(getCurrentId){
+        setSelected(getCurrentId === selected ? null : getCurrentId);
+
+    }
+
+console.log(selected);
+
+    return (
+    <div className='wrapper'>
+        <div className='acccordian'>
+            {
+                data && data.length > 0 ? 
+                data.map(dataItem => <div className='item'> 
+                <div onClick={() => handleSingleSelection(dataItem.id)} className='title'>
+                    <h3>{dataItem.question}</h3>
+                    <span>+</span>
+                </div>
+                {
+                    selected === dataItem.id ?
+                    <div className='content'>{dataItem.answer}</div> 
+                    :null
+                }
+                </div>)
+                : <div> No data found </div>
+            }
+        </div>
+    </div>
+    );  
+}
